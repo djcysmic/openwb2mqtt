0c365a6e93aed9bfe70f4aec2f9010c2fbec2a5c
djcysmic <djcysmic@gmx.de>
2026-09-23 18:49:56 +0200

    Update device retrieval in MQTT message handler
    Refactor message handling to use device_entry directly.

--- custom_components/openwb2mqtt/sensor.py
+++ custom_components/openwb2mqtt/sensor.py
@@ -280,9 +280,11 @@ class openwbSensor(OpenWBBaseEntity, SensorEntity):
             """Handle new MQTT messages."""
             self._attr_native_value = message.payload
             device_registry = dr.async_get(self.hass)
-            device = device_registry.async_get_device(
-                self.device_info.get("identifiers")
-            )
+            device = self.device_entry
+
+            if device is None:
+                return
+
             if self.entity_description.value_fn is not None:
                 self._attr_native_value = self.entity_description.value_fn(
                     self._attr_native_value
